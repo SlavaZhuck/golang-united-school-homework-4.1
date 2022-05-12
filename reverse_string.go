74cd2a11a924b7ce66c999f3dc3e341eb604fd51
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-12 21:59:00 +0300

    add solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,21 @@
 package reverse_string
 
+// package main
+
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	temp := []rune(input)
+	i := 0
+	for _, char := range input {
+		temp[len(input)-1-i] = char
+		i++
+	}
+	return string(temp)
 }
+
+// func main() {
+// 	m := "test_string"
+
+// 	fmt.Println(m)
+// 	fmt.Println(ReverseString(m))
+// }
